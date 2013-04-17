5c815e6110bf54d2c4b87f391e8d85023103668d
XhmikosR <xhmikosr@users.sourceforge.net>
2013-04-17 18:21:42 +0300

    custom-test.js: minor simplification

--- test/custom-test.js
+++ test/custom-test.js
@@ -2,15 +2,13 @@ var vows = require('vows');
 var assert = require('assert');
 var CleanCSS = require('../index');
 
-vows.describe('clean-custom')
-  .addBatch({
-    'imported as function': {
-      topic: function() {
-        return CleanCSS.process;
-      },
-      'should process CSS correctly': function(process) {
-        assert.equal(process('a{  color: #f00;  }'), 'a{color:red}');
-      }
+vows.describe('clean-custom').addBatch({
+  'imported as function': {
+    topic: function() {
+      return CleanCSS.process;
+    },
+    'should process CSS correctly': function(process) {
+      assert.equal(process('a{  color: #f00;  }'), 'a{color:red}');
     }
-  })
-  .export(module);
+  }
+}).export(module);

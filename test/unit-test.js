2b16c06aded94909563c20da5f87a3b6882501e1
GoalSmashers <jakub@goalsmashers.com>
2013-05-27 23:36:45 +0200

    Fixed code formatting issues.

--- test/unit-test.js
+++ test/unit-test.js
@@ -811,11 +811,11 @@ title']",
     ],
     'of a file with a relative resource path': [
       "@import url(test/data/partials/three.css);",
-      ".three{background-image:url(test/data/partials/extra/down.gif)}",
+      ".three{background-image:url(test/data/partials/extra/down.gif)}"
     ],
     'of a file with an absolute resource path': [
       "@import url(test/data/partials/four.css);",
-      ".four{background-image:url(/partials/extra/down.gif)}",
+      ".four{background-image:url(/partials/extra/down.gif)}"
     ],
     'of a file with a resource URI': [
       "@import url(test/data/partials/five.css);",

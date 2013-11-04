ec8986d5bc4cdd8119626de20e8819f1fbea69a4
XhmikosR <xhmikosr@users.sourceforge.net>
2013-11-04 12:09:23 +0200

    Enable JSHint's `eqeq` rule.
    http://www.jshint.com/docs/options/#eqeqeq
    
    This way we are consistent.

--- test/batch-test.js
+++ test/batch-test.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var assert = require('assert');
 var CleanCSS = require('../index');
 
-var lineBreak = process.platform == 'win32' ? /\r\n/g : /\n/g;
+var lineBreak = process.platform === 'win32' ? /\r\n/g : /\n/g;
 
 var batchContexts = function() {
   var context = {};

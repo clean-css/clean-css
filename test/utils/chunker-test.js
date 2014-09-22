29f9c299122f2bccaafdc18745ebbd0698b33dd2
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2014-09-22 03:58:25 +0200

    Extracts Chunker into a module.
    * Adds some simple specs too.

---
+++ test/utils/chunker-test.js
@@ -0,0 +1,29 @@
+var vows = require('vows');
+var assert = require('assert');
+var Chunker = require('../../lib/utils/chunker');
+
+vows.describe(Chunker)
+  .addBatch({
+    'empty string': {
+      topic: new Chunker('', 128),
+      'is empty': function (chunker) {
+        assert.isTrue(chunker.isEmpty());
+      },
+      'has no next': function (chunker) {
+        assert.isUndefined(chunker.next());
+      }
+    },
+    'css': {
+      topic: new Chunker('a{color:red}p{}', 3),
+      'is not empty': function (chunker) {
+        assert.isFalse(chunker.isEmpty());
+      },
+      'breaks at first brace': function (chunker) {
+        assert.equal('a{color:red}', chunker.next());
+      },
+      'breaks at second brace': function (chunker) {
+        assert.equal('p{}', chunker.next());
+      }
+    }
+  })
+  .export(module);

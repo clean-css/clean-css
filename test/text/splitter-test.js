1827c7fd5111b187ebe24b4a779ff0d2167ca1db
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2014-09-24 21:21:57 +0100

    Adds minor tweaks to Splitter.

--- test/text/splitter-test.js
+++ test/text/splitter-test.js
@@ -2,19 +2,21 @@ var vows = require('vows');
 var assert = require('assert');
 var Splitter = require('../../lib/text/splitter');
 
-var split = function (value, expectedValue, separator) {
+function split(value, expectedValue, separator) {
   return function () {
     assert.deepEqual(new Splitter(separator).split(value), expectedValue);
   };
-};
+}
 
-vows.describe('splitter').addBatch({
-  'empty': split('', [''], ','),
-  'simple': split('none', ['none'], ','),
-  'comma separated - level 0': split('#000,#fff,#0f0', ['#000', '#fff', '#0f0'], ','),
-  'comma separated - level 1': split('rgb(0,0,0),#fff', ['rgb(0,0,0)', '#fff'], ','),
-  'comma separated - level 2': split('linear-gradient(0,#fff,rgba(0,0,0)),red', ['linear-gradient(0,#fff,rgba(0,0,0))', 'red'], ','),
-  'space separated - level 0': split('#000 #fff #0f0', ['#000', '#fff', '#0f0'], ' '),
-  'space separated - level 1': split('rgb(0, 0, 0) #fff', ['rgb(0, 0, 0)', '#fff'], ' '),
-  'space separated - level 2': split('linear-gradient(0, #fff, rgba(0, 0, 0)) red', ['linear-gradient(0, #fff, rgba(0, 0, 0))', 'red'], ' ')
-}).export(module);
+vows.describe(Splitter)
+  .addBatch({
+    'empty': split('', [''], ','),
+    'simple': split('none', ['none'], ','),
+    'comma separated - level 0': split('#000,#fff,#0f0', ['#000', '#fff', '#0f0'], ','),
+    'comma separated - level 1': split('rgb(0,0,0),#fff', ['rgb(0,0,0)', '#fff'], ','),
+    'comma separated - level 2': split('linear-gradient(0,#fff,rgba(0,0,0)),red', ['linear-gradient(0,#fff,rgba(0,0,0))', 'red'], ','),
+    'space separated - level 0': split('#000 #fff #0f0', ['#000', '#fff', '#0f0'], ' '),
+    'space separated - level 1': split('rgb(0, 0, 0) #fff', ['rgb(0, 0, 0)', '#fff'], ' '),
+    'space separated - level 2': split('linear-gradient(0, #fff, rgba(0, 0, 0)) red', ['linear-gradient(0, #fff, rgba(0, 0, 0))', 'red'], ' ')
+  })
+  .export(module);

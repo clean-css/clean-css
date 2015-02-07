6ed784a4aae91e5d7f48cf189c24095dfb8062cd
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2015-02-07 19:45:37 +0000

    Fixes #204 - media query merging.
    It adds media query merging with the following set of rules:
    
    * adjacent media with same conditions can be merged;
    * non-adjacent media with same conditions can be merged only if
      any of moved properties is not redefined in-between, or a
      property is redefined with the exactly same value.

---
+++ test/media-queries-test.js
@@ -0,0 +1,91 @@
+var vows = require('vows');
+var assert = require('assert');
+var CleanCSS = require('../index');
+
+vows.describe('media queries')
+  .addBatch({
+    'different ones': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media print{div{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media screen{a{color:red}}@media print{div{display:block}}');
+      }
+    },
+    'other than @media': {
+      topic: new CleanCSS().minify('@font-face{font-family:A}@font-face{font-family:B}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@font-face{font-family:A}@font-face{font-family:B}');
+      }
+    }
+  })
+  .addBatch({
+    'same two adjacent': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media screen{div{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media screen{a{color:red}div{display:block}}');
+      }
+    },
+    'same three adjacent': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media screen{div{display:block}}@media screen{body{width:100%}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media screen{a{color:red}div{display:block}body{width:100%}}');
+      }
+    },
+    'same two with selectors in between': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}body{width:100%}.one{height:100px}@media screen{div{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, 'body{width:100%}.one{height:100px}@media screen{a{color:red}div{display:block}}');
+      }
+    },
+    'same two with other @media in between': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media (min-width:1024px){body{width:100%}}@media screen{div{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media (min-width:1024px){body{width:100%}}@media screen{a{color:red}div{display:block}}');
+      }
+    },
+    'same two with breaking properties in between': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}.one{color:#00f}@media screen{div{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media screen{a{color:red}}.one{color:#00f}@media screen{div{display:block}}');
+      }
+    },
+    'same two with breaking @media in between': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media (min-width:1024px){.one{color:#00f}}@media screen{div{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media screen{a{color:red}}@media (min-width:1024px){.one{color:#00f}}@media screen{div{display:block}}');
+      }
+    },
+    'same two with breaking nested @media in between': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media (min-width:1024px){@media screen{.one{color:#00f}}}@media screen{div{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media screen{a{color:red}}@media (min-width:1024px){@media screen{.one{color:#00f}}}@media screen{div{display:block}}');
+      }
+    },
+    'intermixed': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media (min-width:1024px){p{width:100%}}@media screen{div{display:block}}@media (min-width:1024px){body{height:100%}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media screen{a{color:red}div{display:block}}@media (min-width:1024px){p{width:100%}body{height:100%}}');
+      }
+    },
+    'same two with same values as moved in between': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media (min-width:1024px){.one{color:red}}@media screen{div{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media (min-width:1024px){.one{color:red}}@media screen{a{color:red}div{display:block}}');
+      }
+    }
+  })
+  .addBatch({
+    'further optimizations': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}@media screen{a{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '@media screen{a{color:red;display:block}}');
+      }
+    }
+  })
+  .addBatch({
+    'with comments': {
+      topic: new CleanCSS().minify('@media screen{a{color:red}}/*! a comment */@media screen{a{display:block}}'),
+      'get merged': function(minified) {
+        assert.equal(minified.styles, '/*! a comment */@media screen{a{color:red;display:block}}');
+      }
+    }
+  }).export(module);

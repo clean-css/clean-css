6957bc71feaf76819dfa3ae53320992d87bd6a5b
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2015-03-24 19:41:25 +0000

    Adds an option to Splitter helper to keep separators.

--- test/utils/splitter-test.js
+++ test/utils/splitter-test.js
@@ -2,9 +2,9 @@ var vows = require('vows');
 var assert = require('assert');
 var Splitter = require('../../lib/utils/splitter');
 
-function split(value, expectedValue, separator) {
+function split(value, expectedValue, separator, withSeparator) {
   return function () {
-    assert.deepEqual(new Splitter(separator).split(value), expectedValue);
+    assert.deepEqual(new Splitter(separator).split(value, withSeparator), expectedValue);
   };
 }
 
@@ -18,5 +18,10 @@ vows.describe(Splitter)
     'space separated - level 0': split('#000 #fff #0f0', ['#000', '#fff', '#0f0'], ' '),
     'space separated - level 1': split('rgb(0, 0, 0) #fff', ['rgb(0, 0, 0)', '#fff'], ' '),
     'space separated - level 2': split('linear-gradient(0, #fff, rgba(0, 0, 0)) red', ['linear-gradient(0, #fff, rgba(0, 0, 0))', 'red'], ' ')
+  })
+  .addBatch({
+    'leading space and quote with separator': split(' "Font"', [' "Font"'], ' ', true),
+    'comma separated - level 2 - with separator': split('linear-gradient(0,#fff,rgba(0,0,0)),red', ['linear-gradient(0,#fff,rgba(0,0,0)),', 'red'], ',', true),
+    'space separated - level 2 - with separator': split('linear-gradient(0, #fff, rgba(0, 0, 0)) red', ['linear-gradient(0, #fff, rgba(0, 0, 0)) ', 'red'], ' ', true)
   })
   .export(module);

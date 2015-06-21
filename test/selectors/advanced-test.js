0ed903f04daf26ef6633342010be24b72436e62c
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2015-06-21 15:18:37 +0100

    Cleans up advanced optimizer implementation.
    It gets rid of AdvancedOptimizer class in favour to simpler
    functional style.

--- test/selectors/advanced-test.js
+++ test/selectors/advanced-test.js
@@ -3,34 +3,6 @@ var optimizerContext = require('../test-helper').optimizerContext;
 
 vows.describe('advanced optimizer')
   .addBatch(
-    optimizerContext('@media', {
-      'empty': [
-        '@media (min-width:980px){}',
-        ''
-      ],
-      'whitespace': [
-        ' @media   ( min-width:  980px ){}',
-        ''
-      ],
-      'body': [
-        '@media (min-width:980px){\na\n{color:red}}',
-        '@media (min-width:980px){a{color:red}}'
-      ],
-      'multiple': [
-        '@media screen, print, (min-width:980px){a{color:red}}',
-        '@media screen,print,(min-width:980px){a{color:red}}'
-      ],
-      'nested once': [
-        '@media screen { @media print { a{color:red} } }',
-        '@media screen{@media print{a{color:red}}}'
-      ],
-      'nested twice': [
-        '@media screen { @media print { @media (min-width:980px) { a{color:red} } } }',
-        '@media screen{@media print{@media (min-width:980px){a{color:red}}}}'
-      ]
-    })
-  )
-  .addBatch(
     optimizerContext('advanced on & aggressive merging on', {
       'repeated' : [
         'a{color:red;color:red}',
@@ -61,6 +33,34 @@ vows.describe('advanced optimizer')
         'a{color:red;color:red}'
       ]
     }, { advanced: false })
+  )
+  .addBatch(
+    optimizerContext('@media', {
+      'empty': [
+        '@media (min-width:980px){}',
+        ''
+      ],
+      'whitespace': [
+        ' @media   ( min-width:  980px ){}',
+        ''
+      ],
+      'body': [
+        '@media (min-width:980px){\na\n{color:red}}',
+        '@media (min-width:980px){a{color:red}}'
+      ],
+      'multiple': [
+        '@media screen, print, (min-width:980px){a{color:red}}',
+        '@media screen,print,(min-width:980px){a{color:red}}'
+      ],
+      'nested once': [
+        '@media screen { @media print { a{color:red} } }',
+        '@media screen{@media print{a{color:red}}}'
+      ],
+      'nested twice': [
+        '@media screen { @media print { @media (min-width:980px) { a{color:red} } } }',
+        '@media screen{@media print{@media (min-width:980px){a{color:red}}}}'
+      ]
+    })
   )
   .addBatch(
     optimizerContext('@font-face', {

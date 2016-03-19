clean-css/clean-css

commit 96096402eaaeb4947afa7ecacba0982381a244f4

Fixes #596 - support for !ie IE<8 hack.
!ie fix is like a backslash hack for IE<10.

lib/properties/wrap-for-optimizing.js
@@ -2,6 +2,7 @@ var BACKSLASH_HACK = '\\';
 var IMPORTANT_TOKEN = '!important';
 var STAR_HACK = '*';
 var UNDERSCORE_HACK = '_';
+var BANG_HACK = '!';
 
 function wrapAll(properties) {
   var wrapped = [];
@@ -37,10 +38,14 @@ function hackType(property) {
     type = 'underscore';
   } else if (name[0] == STAR_HACK) {
     type = 'star';
+  } else if (lastValue[0][0] == BANG_HACK && lastValue[0].indexOf('important') == -1) {
+    type = 'bang';
+  } else if (lastValue[0].indexOf(BANG_HACK) > 0 && lastValue[0].indexOf('important') == -1) {
+    type = 'bang';
   } else if (lastValue[0].indexOf(BACKSLASH_HACK) > 0 && lastValue[0].indexOf(BACKSLASH_HACK) == lastValue[0].length - BACKSLASH_HACK.length - 1) {
-    type = 'suffix';
+    type = 'backslash';
   } else if (lastValue[0].indexOf(BACKSLASH_HACK) === 0 && lastValue[0].length == 2) {
-    type = 'suffix';
+    type = 'backslash';
   }
 
   return type;
@@ -61,9 +66,11 @@ function stripPrefixHack(property) {
   property[0][0] = property[0][0].substring(1);
 }
 
-function stripSuffixHack(property) {
+function stripSuffixHack(property, hackType) {
   var lastValue = property[property.length - 1];
-  lastValue[0] = lastValue[0].substring(0, lastValue[0].length - BACKSLASH_HACK.length - 1);
+  lastValue[0] = lastValue[0]
+    .substring(0, lastValue[0].indexOf(hackType == 'backslash' ? BACKSLASH_HACK : BANG_HACK))
+    .trim();
 
   if (lastValue[0].length === 0)
     property.pop();
@@ -77,8 +84,8 @@ function wrapSingle(property) {
   var _hackType = hackType(property);
   if (_hackType == 'star' || _hackType == 'underscore')
     stripPrefixHack(property);
-  else if (_hackType == 'suffix')
-    stripSuffixHack(property);
+  else if (_hackType == 'backslash' || _hackType == 'bang')
+    stripSuffixHack(property, _hackType);
 
   return {
     components: [],

test/integration-test.js
@@ -1566,7 +1566,7 @@ vows.describe('integration tests')
     })
   )
   .addBatch(
-    optimizerContext('IE hacks', {
+    optimizerContext('IE 8 hacks', {
       'star': [
         'a{*color:#fff}',
         'a{*color:#fff}'
@@ -1632,6 +1632,14 @@ vows.describe('integration tests')
         'a{color:hsla(120,100%,50%,0)}'
       ]
     }, { compatibility: 'ie8' })
+  )
+  .addBatch(
+    optimizerContext('IE 7 hacks', {
+      'keeps !ie hack 123': [
+        'a{list-style-type:none;list-style-type:decimal !ie}',
+        'a{list-style-type:none;list-style-type:decimal !ie}'
+      ]
+    }, { compatibility: 'ie7' })
   )
   .addBatch(
     optimizerContext('IE hacks without IE compatibility', {

lib/properties/restore-from-optimizing.js
@@ -4,6 +4,7 @@ var BACKSLASH_HACK = '\\9';
 var IMPORTANT_TOKEN = '!important';
 var STAR_HACK = '*';
 var UNDERSCORE_HACK = '_';
+var BANG_HACK = '!ie';
 
 function restoreImportant(property) {
   property.value[property.value.length - 1][0] += IMPORTANT_TOKEN;
@@ -14,8 +15,10 @@ function restoreHack(property) {
     property.name = UNDERSCORE_HACK + property.name;
   else if (property.hack == 'star')
     property.name = STAR_HACK + property.name;
-  else if (property.hack == 'suffix')
+  else if (property.hack == 'backslash')
     property.value[property.value.length - 1][0] += BACKSLASH_HACK;
+  else if (property.hack == 'bang')
+    property.value[property.value.length - 1][0] += ' ' + BANG_HACK;
 }
 
 function restoreFromOptimizing(properties, simpleMode) {

lib/selectors/simple.js
@@ -299,7 +299,10 @@ function optimizeBody(properties, options) {
     property = _properties[i];
     name = property.name;
 
-    if (property.hack && ((property.hack == 'star' || property.hack == 'underscore') && !options.compatibility.properties.iePrefixHack || property.hack == 'suffix' && !options.compatibility.properties.ieSuffixHack))
+    if (property.hack && (
+        (property.hack == 'star' || property.hack == 'underscore') && !options.compatibility.properties.iePrefixHack ||
+        property.hack == 'backslash' && !options.compatibility.properties.ieSuffixHack ||
+        property.hack == 'bang' && !options.compatibility.properties.ieBangHack))
       property.unused = true;
 
     if (name.indexOf('padding') === 0 && (isNegative(property, 0) || isNegative(property, 1) || isNegative(property, 2) || isNegative(property, 3)))

lib/utils/compatibility.js
@@ -10,6 +10,7 @@ var DEFAULTS = {
       backgroundOriginMerging: false, // background-origin to shorthand
       backgroundSizeMerging: false, // background-size to shorthand
       colors: true, // any kind of color transformations, like `#ff00ff` to `#f0f` or `#fff` into `red`
+      ieBangHack: false, // !ie suffix hacks on IE<8
       iePrefixHack: false, // underscore / asterisk prefix hacks on IE
       ieSuffixHack: true, // \9 suffix hacks on IE6-9
       merging: true, // merging properties into one
@@ -44,6 +45,7 @@ var DEFAULTS = {
       backgroundOriginMerging: false,
       backgroundSizeMerging: false,
       colors: true,
+      ieBangHack: false,
       iePrefixHack: true,
       ieSuffixHack: true,
       merging: false,
@@ -78,6 +80,7 @@ var DEFAULTS = {
       backgroundOriginMerging: false,
       backgroundSizeMerging: false,
       colors: true,
+      ieBangHack: true,
       iePrefixHack: true,
       ieSuffixHack: true,
       merging: false,

test/properties/wrap-for-optimizing-test.js
@@ -140,7 +140,7 @@ vows.describe(wrapForOptimizing)
         assert.deepEqual(wrapped[0].value, [['0']]);
       },
       'is a hack': function (wrapped) {
-        assert.equal(wrapped[0].hack, 'suffix');
+        assert.equal(wrapped[0].hack, 'backslash');
       }
     },
     'backslash hack - single value': {
@@ -168,7 +168,41 @@ vows.describe(wrapForOptimizing)
         assert.deepEqual(wrapped[0].value, [['0']]);
       },
       'is a hack': function (wrapped) {
-        assert.equal(wrapped[0].hack, 'suffix');
+        assert.equal(wrapped[0].hack, 'backslash');
+      }
+    },
+    'bang hack': {
+      'topic': function () {
+        return wrapForOptimizing([[['margin'], ['0'], ['!ie']]]);
+      },
+      'has one wrap': function (wrapped) {
+        assert.lengthOf(wrapped, 1);
+      },
+      'has right value': function (wrapped) {
+        assert.deepEqual(wrapped[0].value, [['0']]);
+      },
+      'is a hack': function (wrapped) {
+        assert.equal(wrapped[0].hack, 'bang');
+      },
+      'is not important': function (wrapped) {
+        assert.isFalse(wrapped[0].important);
+      }
+    },
+    'bang hack - space between values': {
+      'topic': function () {
+        return wrapForOptimizing([[['margin'], ['0 !ie']]]);
+      },
+      'has one wrap': function (wrapped) {
+        assert.lengthOf(wrapped, 1);
+      },
+      'has right value': function (wrapped) {
+        assert.deepEqual(wrapped[0].value, [['0']]);
+      },
+      'is a hack': function (wrapped) {
+        assert.equal(wrapped[0].hack, 'bang');
+      },
+      'is not important': function (wrapped) {
+        assert.isFalse(wrapped[0].important);
       }
     },
     'source map': {
@@ -185,7 +219,7 @@ vows.describe(wrapForOptimizing)
         assert.isTrue(wrapped[0].important);
       },
       'is a hack': function (wrapped) {
-        assert.equal(wrapped[0].hack, 'suffix');
+        assert.equal(wrapped[0].hack, 'backslash');
       }
     }
   })

test/selectors/simple-test.js
@@ -365,11 +365,15 @@ vows.describe('simple optimizations')
       'backslash': [
         'a{width:101px\\9}',
         [['width', '101px\\9']]
+      ],
+      'bang': [
+        'a{color:red !ie}',
+        null
       ]
     })
   )
   .addBatch(
-    propertyContext('ie hacks in compatibility mode', {
+    propertyContext('ie hacks in IE8 mode', {
       'underscore': [
         'a{_width:101px}',
         [['_width', '101px']]
@@ -381,8 +385,32 @@ vows.describe('simple optimizations')
       'backslash': [
         'a{width:101px\\9}',
         [['width', '101px\\9']]
+      ],
+      'bang': [
+        'a{color:red !ie}',
+        null
       ]
     }, { compatibility: 'ie8' })
+  )
+  .addBatch(
+    propertyContext('ie hacks in IE7 mode', {
+      'underscore': [
+        'a{_width:101px}',
+        [['_width', '101px']]
+      ],
+      'star': [
+        'a{*width:101px}',
+        [['*width', '101px']]
+      ],
+      'backslash': [
+        'a{width:101px\\9}',
+        [['width', '101px\\9']]
+      ],
+      'bang': [
+        'a{color:red !ie}',
+        [['color', 'red !ie']]
+      ]
+    }, { compatibility: 'ie7' })
   )
   .addBatch(
     propertyContext('important', {

test/utils/compatibility-test.js
@@ -14,6 +14,7 @@ vows.describe(Compatibility)
         assert.isFalse(options.properties.backgroundClipMerging);
         assert.isFalse(options.properties.backgroundOriginMerging);
         assert.isFalse(options.properties.backgroundSizeMerging);
+        assert.isFalse(options.properties.ieBangHack);
         assert.isFalse(options.properties.iePrefixHack);
         assert.isTrue(options.properties.ieSuffixHack);
         assert.isTrue(options.properties.merging);
@@ -52,6 +53,7 @@ vows.describe(Compatibility)
         assert.isFalse(options.properties.backgroundOriginMerging);
         assert.isFalse(options.properties.backgroundSizeMerging);
         assert.isTrue(options.properties.colors);
+        assert.isFalse(options.properties.ieBangHack);
         assert.isFalse(options.properties.iePrefixHack);
         assert.isTrue(options.properties.ieSuffixHack);
         assert.isTrue(options.properties.merging);
@@ -83,6 +85,7 @@ vows.describe(Compatibility)
         assert.isFalse(options.properties.backgroundOriginMerging);
         assert.isFalse(options.properties.backgroundSizeMerging);
         assert.isTrue(options.properties.colors);
+        assert.isFalse(options.properties.ieBangHack);
         assert.isTrue(options.properties.iePrefixHack);
         assert.isTrue(options.properties.ieSuffixHack);
         assert.isFalse(options.properties.merging);
@@ -113,6 +116,7 @@ vows.describe(Compatibility)
         assert.isFalse(options.properties.backgroundOriginMerging);
         assert.isFalse(options.properties.backgroundSizeMerging);
         assert.isTrue(options.properties.colors);
+        assert.isTrue(options.properties.ieBangHack);
         assert.isTrue(options.properties.iePrefixHack);
         assert.isTrue(options.properties.ieSuffixHack);
         assert.isFalse(options.properties.merging);
@@ -153,6 +157,7 @@ vows.describe(Compatibility)
         assert.isFalse(options.properties.backgroundOriginMerging);
         assert.isFalse(options.properties.backgroundSizeMerging);
         assert.isTrue(options.properties.colors);
+        assert.isFalse(options.properties.ieBangHack);
         assert.isFalse(options.properties.iePrefixHack);
         assert.isTrue(options.properties.ieSuffixHack);
         assert.isFalse(options.properties.merging);
@@ -183,6 +188,7 @@ vows.describe(Compatibility)
         assert.isFalse(options.properties.backgroundClipMerging);
         assert.isFalse(options.properties.backgroundOriginMerging);
         assert.isFalse(options.properties.backgroundSizeMerging);
+        assert.isFalse(options.properties.ieBangHack);
         assert.isTrue(options.properties.iePrefixHack);
         assert.isTrue(options.properties.ieSuffixHack);
         assert.isTrue(options.properties.merging);
@@ -213,6 +219,7 @@ vows.describe(Compatibility)
         assert.isFalse(options.properties.backgroundClipMerging);
         assert.isFalse(options.properties.backgroundOriginMerging);
         assert.isFalse(options.properties.backgroundSizeMerging);
+        assert.isFalse(options.properties.ieBangHack);
         assert.isTrue(options.properties.iePrefixHack);
         assert.isTrue(options.properties.ieSuffixHack);
         assert.isTrue(options.properties.merging);

commit 3395d93711eca4f231a66a83fc557cc0581ef4b2

Don't skip !important rule with capital letters
Fix #745

In v3.3.10 works ok, fails in v3.4.10

The problem was introduced in 96096402eaaeb4947afa7ecacba0982381a244f4

Prevent match !importantE with !important

lib/properties/wrap-for-optimizing.js
@@ -1,5 +1,8 @@
 var BACKSLASH_HACK = '\\';
-var IMPORTANT_TOKEN = '!important';
+var IMPORTANT_WORD = 'important';
+var IMPORTANT_TOKEN = '!'+IMPORTANT_WORD;
+var IMPORTANT_WORD_MATCH = new RegExp(IMPORTANT_WORD+'$', 'i');
+var IMPORTANT_TOKEN_MATCH = new RegExp(IMPORTANT_TOKEN+'$', 'i');
 var STAR_HACK = '*';
 var UNDERSCORE_HACK = '_';
 var BANG_HACK = '!';
@@ -38,9 +41,9 @@ function hackType(property) {
     type = 'underscore';
   } else if (name[0] == STAR_HACK) {
     type = 'star';
-  } else if (lastValue[0][0] == BANG_HACK && lastValue[0].indexOf('important') == -1) {
+  } else if (lastValue[0][0] == BANG_HACK && !lastValue[0].match(IMPORTANT_WORD_MATCH)) {
     type = 'bang';
-  } else if (lastValue[0].indexOf(BANG_HACK) > 0 && lastValue[0].indexOf('important') == -1) {
+  } else if (lastValue[0].indexOf(BANG_HACK) > 0 && !lastValue[0].match(IMPORTANT_WORD_MATCH)) {
     type = 'bang';
   } else if (lastValue[0].indexOf(BACKSLASH_HACK) > 0 && lastValue[0].indexOf(BACKSLASH_HACK) == lastValue[0].length - BACKSLASH_HACK.length - 1) {
     type = 'backslash';
@@ -52,14 +55,18 @@ function hackType(property) {
 }
 
 function isImportant(property) {
-  return property.length > 1 ?
-    property[property.length - 1][0].indexOf(IMPORTANT_TOKEN) > 0 :
-    false;
+  if (property.length > 1) {
+    var p = property[property.length - 1][0];
+    if (typeof(p) === 'string') {
+      return IMPORTANT_TOKEN_MATCH.test(p);
+    }
+  }
+  return false;
 }
 
 function stripImportant(property) {
   if (property.length > 0)
-    property[property.length - 1][0] = property[property.length - 1][0].replace(IMPORTANT_TOKEN, '');
+    property[property.length - 1][0] = property[property.length - 1][0].replace(IMPORTANT_TOKEN_MATCH, '');
 }
 
 function stripPrefixHack(property) {

lib/tokenizer/extract-properties.js
@@ -5,6 +5,11 @@ var FORWARD_SLASH = '/';
 
 var AT_RULE = 'at-rule';
 
+var IMPORTANT_WORD = 'important';
+var IMPORTANT_TOKEN = '!'+IMPORTANT_WORD;
+var IMPORTANT_WORD_MATCH = new RegExp('^'+IMPORTANT_WORD+'$', 'i');
+var IMPORTANT_TOKEN_MATCH = new RegExp('^'+IMPORTANT_TOKEN+'$', 'i');
+
 function selectorName(value) {
   return value[0];
 }
@@ -153,14 +158,14 @@ function extractProperties(string, selectors, context) {
       }
 
       var pos = body.length - 1;
-      if (trimmed == 'important' && body[pos][0] == '!') {
+      if (IMPORTANT_WORD_MATCH.test(trimmed) && body[pos][0] == '!') {
         context.track(trimmed);
-        body[pos - 1][0] += '!important';
+        body[pos - 1][0] += IMPORTANT_TOKEN;
         body.pop();
         continue;
       }
 
-      if (trimmed == '!important' || (trimmed == 'important' && body[pos][0][body[pos][0].length - 1] == '!')) {
+      if (IMPORTANT_TOKEN_MATCH.test(trimmed) || (IMPORTANT_WORD_MATCH.test(trimmed) && body[pos][0][body[pos][0].length - 1] == '!')) {
         context.track(trimmed);
         body[pos][0] += trimmed;
         continue;

test/integration-test.js
@@ -1882,6 +1882,14 @@ vows.describe('integration tests')
       'space between ! and important': [
         'body{background-color:#fff  ! important}',
         'body{background-color:#fff!important}'
+      ],
+      'Capital letters': [
+        'a{color: red !ImporTant }',
+        'a{color:red!important}'
+      ],
+      'Match word': [
+        'a{color: red !importante }',
+        ''
       ]
     })
   )
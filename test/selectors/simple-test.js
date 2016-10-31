dcaa241d90a4842276b0fb5296d6e8f88ca5b8fe
vlakoff <vlakoff@gmail.com>
2016-10-31 22:32:26 +0100

    Add test when precision not castable to int

--- test/selectors/simple-test.js
+++ test/selectors/simple-test.js
@@ -501,6 +501,22 @@ vows.describe('simple optimizations')
     }, { roundingPrecision: -1 })
   )
   .addBatch(
+    propertyContext('rounding disabled when option value not castable to int', {
+      'pixels': [
+        'a{transform:translateY(123.31135px)}',
+        [['transform', 'translateY(123.31135px)']]
+      ],
+      'percents': [
+        'a{left:20.1231%}',
+        [['left', '20.1231%']]
+      ],
+      'ems': [
+        'a{left:1.1231em}',
+        [['left', '1.1231em']]
+      ]
+    }, { roundingPrecision: '\'-1\'' })
+  )
+  .addBatch(
     propertyContext('units', {
       'pixels': [
         'a{width:0px}',

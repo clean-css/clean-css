98e295563583b97cfd93b17a8f60e0721194a64c
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2016-11-16 12:01:13 +0100

    Bumps online version to clean-css 3.4.21.

--- docs/js/optimizer-worker.js
+++ docs/js/optimizer-worker.js
@@ -5,7 +5,7 @@ onmessage = function(event) {
     case 'initialize':
       if (!initialized) {
         initialized = true
-        importScripts('//jakubpawlowicz.github.io/clean-css-builds/v3.4.20.js')
+        importScripts('//jakubpawlowicz.github.io/clean-css-builds/v3.4.21.js')
       }
       break
     case 'optimize':

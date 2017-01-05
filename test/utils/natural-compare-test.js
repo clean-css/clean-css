7b2b3572406adaef8cc0097c93a965c11c4f9f1f
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2017-01-05 12:35:17 +0100

    See #425 - adds natural sorting algorithm.
    Why:
    
    * We need it to sort selectors accordingly.

---
+++ test/utils/natural-compare-test.js
@@ -0,0 +1,46 @@
+var assert = require('assert');
+
+var vows = require('vows');
+
+var naturalCompare = require('../../lib/utils/natural-compare');
+
+vows.describe(naturalCompare)
+  .addBatch({
+    'numbers': {
+      'topic': [2, 3, 1, 5, 4, 11, 22],
+      'are sorted': function (list) {
+        assert.deepEqual(list.sort(naturalCompare), [1, 2, 3, 4, 5, 11, 22]);
+      }
+    },
+    'numbers and strings': {
+      'topic': ['2', 3, 1, 5, '4', '11', '22'],
+      'are sorted': function (list) {
+        assert.deepEqual(list.sort(naturalCompare), [1, '2', 3, '4', 5, '11', '22']);
+      }
+    },
+    'strings': {
+      'topic': ['2', '3', '1', '5', '4', '11', '22'],
+      'are sorted': function (list) {
+        assert.deepEqual(list.sort(naturalCompare), ['1', '2', '3', '4', '5', '11', '22']);
+      }
+    },
+    'strings with same prefix': {
+      'topic': ['x2', 'x3', 'x1', 'x5', 'x4', 'x11', 'x22'],
+      'are sorted': function (list) {
+        assert.deepEqual(list.sort(naturalCompare), ['x1', 'x2', 'x3', 'x4', 'x5', 'x11', 'x22']);
+      }
+    },
+    'strings with different prefixes': {
+      'topic': ['x2', 'x3', 'x1', 'y5', 'y4', 'y11', 'y22'],
+      'are sorted': function (list) {
+        assert.deepEqual(list.sort(naturalCompare), ['x1', 'x2', 'x3', 'y4', 'y5', 'y11', 'y22']);
+      }
+    },
+    'strings with different prefixes and suffixes of different length': {
+      'topic': ['x2a1', 'x3', 'x1b2', 'x2a2', 'x3a1', 'x33', 'x1b21'],
+      'are sorted': function (list) {
+        assert.deepEqual(list.sort(naturalCompare), ['x1b2', 'x1b21', 'x2a1', 'x2a2', 'x3', 'x3a1', 'x33']);
+      }
+    }
+  })
+  .export(module);

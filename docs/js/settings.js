a1f6e36f6265695833c3c4f97473a5611dc6ad36
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2017-04-24 08:03:06 +0200

    See #906 - sets optimization options inline.
    Why:
    
    * It's not fully intuitive to have an `Apply` button at the bottom of
      the form and require users to find it;
    * instead options are set now automatically upon change.

--- docs/js/settings.js
+++ docs/js/settings.js
@@ -3,10 +3,6 @@
 
   function show(settingsForm) {
     return function (event) {
-      if (event.target.classList.contains('settings__option--apply')) {
-        return
-      }
-
       if (settingsForm.classList.contains('settings--collapsed')) {
         event.preventDefault()
         settingsForm.classList.remove('settings--collapsed')
@@ -22,20 +18,20 @@
     }
   }
 
-  function hide(settingsForm) {
-    return function (event) {
-      event.preventDefault()
-      settingsForm.classList.add('settings--collapsed')
-    }
-  }
-
   function setOptionsFrom(settingsForm) {
     var compatibilityNode = settingsForm.querySelector('.js-settings-compatibility')
     var formatOptionsContainer = settingsForm.querySelector('.js-settings-format-options')
     var level1OptionsContainer = settingsForm.querySelector('.js-settings-level-1-options')
     var level2OptionsContainer = settingsForm.querySelector('.js-settings-level-2-options')
 
-    return function () {
+    return function (event) {
+      var viaInputNode = event && event.target.classList.contains('js-settings-option')
+      var viaLabelNode = event && event.target.control && event.target.control.classList.contains('js-settings-option')
+
+      if (event && !viaInputNode && !viaLabelNode) {
+        return
+      }
+
       Optimizer.options = {
         compatibility: compatibilityNode.value,
         format: formattingEnabled(settingsForm) ?
@@ -121,14 +117,13 @@
     var level1Checkbox = settingsForm.querySelector('.js-settings-level-1')
     var level2Checkbox = settingsForm.querySelector('.js-settings-level-2')
     var formattingCheckbox = settingsForm.querySelector('.js-settings-format')
-    var applySettingsButton = settingsForm.querySelector('.js-settings-apply')
 
     settingsForm.addEventListener('click', show(settingsForm), false)
+    settingsForm.addEventListener('click', setOptionsFrom(settingsForm), false)
+    settingsForm.addEventListener('blur', setOptionsFrom(settingsForm), false)
     level1Checkbox.addEventListener('click', toggleOptions(level1Checkbox), false)
     level2Checkbox.addEventListener('click', toggleOptions(level2Checkbox), false)
     formattingCheckbox.addEventListener('click', toggleOptions(formattingCheckbox), false)
-    applySettingsButton.addEventListener('click', hide(settingsForm), false)
-    applySettingsButton.addEventListener('click', setOptionsFrom(settingsForm), false)
 
     setOptionsFrom(settingsForm)()
   })

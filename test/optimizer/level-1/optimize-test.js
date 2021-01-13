77a8dddc465cdb9c707663e9f45469a4f3e035c4
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2021-01-13 14:31:43 +0100

    See #1186 - cherry picks fix from 4.2 branch.

--- test/optimizer/level-1/optimize-test.js
+++ test/optimizer/level-1/optimize-test.js
@@ -416,7 +416,12 @@ vows.describe('level 1 optimizations')
       'hsla with variables': [
         '.block{color: hsl(0, 0%, calc((var(--button_color_l) - 65) * -100%))}',
         '.block{color:hsl(0,0%,calc((var(--button_color_l) - 65) * -100%))}'
+      ],
+      'rgba inside a function': [
+        '.block{background-image:linear-gradient(to right,rgba(255,255,255,0),rgba(255,255,255,1))}',
+        '.block{background-image:linear-gradient(to right,rgba(255,255,255,0),#fff)}'
       ]
+
     }, { level: 1 })
   )
   .addBatch(

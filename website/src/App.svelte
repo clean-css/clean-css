2e4359e0d559b01ab530b7c9ae7865c9505fdf34
johannchopin <johannchopin@pm.me>
2021-06-19 14:45:26 +0200

    add footer

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,14 +1,11 @@
 <script>
 	import Header from "./Header.svelte"
-
-	export let name
+	import Footer from "./Footer.svelte"
 </script>
 
 <main>
 	<Header />
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<button type="button" class="btn btn-primary">Primary</button>
+	<Footer />
 </main>
 
 <style>

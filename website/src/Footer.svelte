bf0a58261ece141b563bcb593b296f51e9c424a8
johannchopin <johannchopin@pm.me>
2021-06-19 22:29:54 +0200

    fix: remove final dot from linkl

--- website/src/Footer.svelte
+++ website/src/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="text-center mt-5 mb-2">
-  Hosted with <span>❤️&nbsp;</span> on <a href="//pages.github.com/">GitHub Pages.</a>
+  Hosted with <span>❤️&nbsp;</span> on <a href="//pages.github.com/">GitHub Pages</a>.
 </footer>
 
 <style>

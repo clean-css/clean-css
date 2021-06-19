1acf15592cb31babd5b26532ab2721ecac1e367c
johannchopin <johannchopin@pm.me>
2021-06-19 14:22:51 +0200

    add Icon component

--- website/rollup.config.js
+++ website/rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import inlineSvg from 'rollup-plugin-inline-svg';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -43,6 +44,9 @@ export default {
 				dev: !production
 			}
 		}),
+
+		inlineSvg(),
+
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
---
+++ website/src/Icon/Icon.svelte
@@ -0,0 +1,34 @@
+<script>
+  import gearHighlighted from './icons/gear-highlighted.svg'
+  import gear from './icons/gear.svg'
+  import logo from './icons/logo.svg'
+  import spinner from './icons/spinner.svg'
+
+  let className = ''
+
+  export let name = 'gear'
+  export { className as class }
+
+  const icons = {
+    'gear-highlighted': gearHighlighted,
+    gear,
+    logo,
+    spinner
+  }
+</script>
+
+<style lang="scss">
+  .icon {
+    display: flex;
+    width: 1em;
+    height: 1em;
+  }
+
+  .icon :global(svg) {
+    width: 1em;
+  }
+</style>
+
+<i class={`icon ${className}`}>
+  {@html icons[name]}
+</i>

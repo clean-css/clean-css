0d3b8ea120ba75dfc81250fe6d6b44987e8e0b53
johannchopin <johannchopin@pm.me>
2021-06-19 22:07:41 +0200

    refactor: byteCount utils

--- website/src/utils.js
+++ website/src/utils.js
@@ -1,4 +1,4 @@
 export const setClipboard = (text) => { navigator.clipboard.writeText(text) }
 export const byteCount = (s) => {
-  return encodeURI(s).split(/%..|./).length - 1;
+  return new Blob([s]).size
 }

8026dbda605570c5e4c9ae0bee4048634346b32f
johannchopin <johannchopin@pm.me>
2021-07-11 12:05:03 +0200

    remove: website folder

--- website/src/main.js
+++
@@ -1,9 +0,0 @@
-import App from './App.svelte'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-const app = new App({
-  target: document.body
-})
-
-export default app
--- website/src/utils.js
+++
@@ -1,4 +0,0 @@
-export const setClipboard = (text) => { navigator.clipboard.writeText(text) }
-export const deepCopyObject = (object) => {
-  return JSON.parse(JSON.stringify(object))
-}
